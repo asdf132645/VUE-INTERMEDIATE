1821f3508ce0b64f9915f1edfc21029cf48aeaae
kjy <amber9291@naver.com>
2021-02-06 15:31:36 +0900

    .

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -3,11 +3,12 @@
     <TodoHeader></TodoHeader>
     <!-- 하위컴포넌트에서 발생시킨 이벤트 이름 = "이벤트 이름"  -->
     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
-    <TodoList v-bind:propsdata="todoItems" 
+    <TodoList 
+              v-bind:propsdata="todoItems" 
               v-on:removeItem="removeOneItem" 
               v-on:toggleItem="toggleOne"
-              ></TodoList>
-    <TodoFooter></TodoFooter>
+    ></TodoList>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -20,30 +21,35 @@ import TodoFooter from './components/TodoFooter.vue'
 
 
 export default {
-  data : function(){
+  data(){
     return{
       todoItems:[]
     }
   },
   methods:{
-    addOneItem: function(todoItem){
-        var obj = {completed: false, item: todoItem};
+    addOneItem(todoItem){
+        const obj = {completed: false, item: todoItem};
         localStorage.setItem(todoItem, JSON.stringify(obj));
         this.todoItems.push(obj);   
     },
-    removeOneItem: function(todoItem, index){
+    removeOneItem(todoItem, index){
         localStorage.removeItem(todoItem);
         this.todoItems.splice(index, 1);
     },
-    toggleOne: function(todoItem, index){
-      todoItem.completed = !todoItem.completed;
+    toggleOne(todoItem, index){
+      this.todoItems[index].completed = !this.todoItems[index].completed
+
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems(){
+      localStorage.clear();
+      this.todoItems = [];
     }
   },
-  created: function(){
+  created(){
             if(localStorage.length > 0){
-                for(var i = 0; i < localStorage.length; i ++){
+                for(let i = 0; i < localStorage.length; i ++){
                     if(localStorage.key(i) !== "loglevel:webpack-dev-server"){
                         this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
                     }
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,29 +5,48 @@
       <span class="addContainer">
             <i class="fas fa-plus" v-on:click="addTodo">추가</i>
       </span>
+       <Modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">
+                헤더
+                <button @click="showModal = false">닫기</button>
+            </h3>
+            <h3 slot="body">
+                s
+            </h3>
+            <!-- <h3 slot="footer">
+                
+            </h3> -->
+      </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
-    data : function(){
+    data(){
         return {
-            newTodoItem: ""
+            newTodoItem: "",
+            showModal: false
         }
     },
     methods: {
-        addTodo: function(){
+        addTodo(){
             if(this.newTodoItem !== ''){
-                // this.$emit('이벤트 이름', 인자1, 인자2, ...)
+                // this.$emit('이벤트 이름', 인자1, 인자2, ..)
                 //상위컴포넌트로 보내기 $emit
                 this.$emit('addTodoItem', this.newTodoItem);
                 this.clearInput();
+            }else{
+                this.showModal = !this.showModal
             }
             
         },
-        clearInput: function(){
+        clearInput(){
             this.newTodoItem = "";
         }
+    },
+    components: {
+        Modal : Modal
     }
 }
 </script>
